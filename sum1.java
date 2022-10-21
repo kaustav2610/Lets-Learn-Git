755efc9d327c618cc94a16cd1cc38cb8698885a3
Kaustav Kumar <kaustav2610@gmail.com>
2022-10-21 20:28:35 +0530

    Second for sum

--- sum1.java
+++ sum1.java
@@ -2,7 +2,7 @@ public class sum1 {
     public static void main(String[] args) {
         int a=54;
         int b=45;
-        System.out.println(a+b);
+        System.out.println("Thr Sum is "+(a+b));
 
     }
     
